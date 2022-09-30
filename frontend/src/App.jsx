7ddd78de7804fe270d52467a2ccd588f01e5660c
MiguelBarreraDev <barreram987@gmail.com>
2022-09-30 14:21:26 -0500

    Add: Contains the implementation of the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,25 @@ import { AuthGuard } from './guards'
 import CssBaseline from '@mui/material/CssBaseline'
 import { SignUp } from './pages/SingUp'
 import { Header } from './components/Common/Header'
+import { Provider } from 'react-redux'
+import { store } from './redux'
 
 function App () {
   return (
     <>
-      <CssBaseline />
-      <Header />
-      <RoutesWithNotFound>
-        <Route path='/' element={<Navigate to={PublicRoutes.LOGIN.route} />} />
-        <Route path={PublicRoutes.LOGIN.route} element={<Login />} />
-        <Route path={PublicRoutes.SIGNUP.route} element={<SignUp />} />
-        <Route element={<AuthGuard />}>
-          <Route path={`${PrivateRoutes.PRIVATE.route}/*`} element={<Private />} />
-        </Route>
-        <Route path='/*' element={<>Not Found</>} />
-      </RoutesWithNotFound>
+      <Provider store={store}>
+        <CssBaseline />
+        <Header />
+        <RoutesWithNotFound>
+          <Route path='/' element={<Navigate to={PublicRoutes.LOGIN.route} />} />
+          <Route path={PublicRoutes.LOGIN.route} element={<Login />} />
+          <Route path={PublicRoutes.SIGNUP.route} element={<SignUp />} />
+          <Route element={<AuthGuard />}>
+            <Route path={`${PrivateRoutes.PRIVATE.route}/*`} element={<Private />} />
+          </Route>
+          <Route path='/*' element={<>Not Found</>} />
+        </RoutesWithNotFound>
+      </Provider>
     </>
   )
 }
