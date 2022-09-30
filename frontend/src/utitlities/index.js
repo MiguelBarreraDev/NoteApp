aa9d7a46673678dfe54e7b6fdf64bc437743afc3
MiguelBarreraDev <barreram987@gmail.com>
2022-09-30 14:45:38 -0500

    Add: Functions, components, data, etc., that will be useful in the project

--- frontend/src/utitlities/index.js
+++ frontend/src/utitlities/index.js
@@ -1,3 +1,4 @@
 export { default as RoutesWithNotFound } from './RoutesWithNotFound'
 export { default as toList } from './toList'
 export { default as convertBase } from './convertBase'
+export { default as loadAbort } from './loadAbort.utility'
