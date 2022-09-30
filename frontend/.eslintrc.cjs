21278e9399e5b678d66c1fac0d208408864f1e91
MiguelBarreraDev <barreram987@gmail.com>
2022-09-30 18:39:15 -0500

    Add: Configuration of the linter(Eslint)

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard'
+  ],
+  overrides: [{
+    files: ['*.js', '*.cjs', '*.jsx']
+  }],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 'off'
+  }
+}
