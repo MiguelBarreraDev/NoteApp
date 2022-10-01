c3b33381c34a08d43b3aed50cdb1db1715af5574
MiguelBarreraDev <barreram987@gmail.com>
2022-10-01 04:43:12 -0500

    Add: Home route to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { Route, Navigate } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { PrivateRoutes, PublicRoutes } from './config/routes'
-import { Login } from './pages/Login'
-import { Private } from './pages/Private'
 import { RoutesWithNotFound } from './utitlities'
 import { AuthGuard } from './guards'
 import CssBaseline from '@mui/material/CssBaseline'
-import { SignUp } from './pages/SingUp'
 import { Header } from './components/Common/Header'
 import { Provider } from 'react-redux'
 import { store } from './redux'
+import { Home, Login, Private, SignUp } from './pages'
 
 function App () {
   return (
@@ -18,7 +16,7 @@ function App () {
         <CssBaseline />
         <Header />
         <RoutesWithNotFound>
-          <Route path='/' element={<Navigate to={PublicRoutes.LOGIN.route} />} />
+          <Route path='/' element={<Home />} />
           <Route path={PublicRoutes.LOGIN.route} element={<Login />} />
           <Route path={PublicRoutes.SIGNUP.route} element={<SignUp />} />
           <Route element={<AuthGuard />}>
