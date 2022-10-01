dd764a6f9b13beee65f52b35f80ec6fa059e3e77
MiguelBarreraDev <barreram987@gmail.com>
2022-10-01 04:47:36 -0500

    Update: Import component styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
 import { ThemeProvider } from '@mui/material/styles'
 import { lightTheme } from './assets/themes/lightTheme'
 import { darkTheme } from './assets/themes/darkTheme'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={lightTheme}>
