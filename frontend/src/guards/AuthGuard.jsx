0ff1381e259bd305d3efe6a40d1cc83121b58886
MiguelBarreraDev <barreram987@gmail.com>
2022-10-02 04:22:26 -0500

    Update: For testing the login action

--- frontend/src/guards/AuthGuard.jsx
+++ frontend/src/guards/AuthGuard.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 
 export default function AuthGuard () {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState(true)
 
   return user
     ? <Outlet />
