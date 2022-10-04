aac83e3812b09aac40e0b68f046a7ce79fd580d2
MiguelBarreraDev <barreram987@gmail.com>
2022-10-03 19:20:50 -0500

    Add: Lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,37 @@
 import './App.css'
 import { Route } from 'react-router-dom'
 import { PrivateRoutes, PublicRoutes } from './config/routes'
-import { RoutesWithNotFound } from './utitlities'
-import { AuthGuard } from './guards'
+import { RoutesWithNotFound, toList } from './utitlities'
+import { AuthGuard, PublicGuard } from './guards'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Header } from './components/Common/Header'
 import { Provider } from 'react-redux'
 import { store } from './redux'
-import { Home, Login, Private, SignUp } from './pages'
+import { lazy, Suspense } from 'react'
+
+const Private = lazy(() => import('@/page/Private/Private'))
 
 function App () {
+  const setPublicRoute = ({ route, key, Component }) => (
+    <Route key={key} path={route} element={<Component />} />
+  )
+
   return (
     <>
-      <Provider store={store}>
-        <CssBaseline />
-        <Header />
-        <RoutesWithNotFound>
-          <Route path='/' element={<Home />} />
-          <Route path={PublicRoutes.LOGIN.route} element={<Login />} />
-          <Route path={PublicRoutes.SIGNUP.route} element={<SignUp />} />
-          <Route element={<AuthGuard />}>
-            <Route path={`${PrivateRoutes.PRIVATE.route}/*`} element={<Private />} />
-          </Route>
-        </RoutesWithNotFound>
-      </Provider>
+      <Suspense fallback={<>Loading...</>}>
+        <Provider store={store}>
+          <CssBaseline />
+          <Header />
+          <RoutesWithNotFound>
+            <Route element={<PublicGuard />}>
+              {toList(PublicRoutes).map(setPublicRoute)}
+            </Route>
+            <Route element={<AuthGuard />}>
+              <Route path={`${PrivateRoutes.PRIVATE.route}/*`} element={<Private />} />
+            </Route>
+          </RoutesWithNotFound>
+        </Provider>
+      </Suspense>
     </>
   )
 }
