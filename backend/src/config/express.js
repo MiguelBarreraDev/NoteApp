d1ed38d29177af8d77575bbb478222c94c914ca3
MiguelBarreraDev <barreram987@gmail.com>
2022-10-04 21:17:08 -0500

    Add: morgan and cors middlewares

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -1,10 +1,16 @@
 import userRouter from '#routes/user.routes.js'
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const expressApp = express()
 
 // Middlewares
+expressApp.use(morgan('tiny'))
 expressApp.use(express.json())
+expressApp.use(cors({
+  origin: 'http://localhost:5173'
+}))
 
 // Routes
 expressApp.use('/users', userRouter)
