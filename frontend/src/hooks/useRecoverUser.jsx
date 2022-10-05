152bdb6bde1cda267fbc0d3ce1b7c9df1692501d
MiguelBarreraDev <barreram987@gmail.com>
2022-10-04 21:15:12 -0500

    Add: Validate user identity before uploading the app

---
+++ frontend/src/hooks/useRecoverUser.jsx
@@ -0,0 +1,37 @@
+import { PublicRoutes } from '@/config'
+import { updateUser } from '@/redux/states'
+import { profileService } from '@/services'
+import { ls } from '@/utitlities'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import useFetchAndLoad from './useFetchAndLoad'
+
+export default function useRecoverUser () {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { callEndpoint } = useFetchAndLoad()
+  const [loading, setLoading] = useState()
+
+  useEffect(() => {
+    const jwt = ls.getItem('jwt')
+    const recoveryUser = async () => {
+      const { data } = await callEndpoint(profileService({ jwt }))
+
+      if (!data) {
+        setLoading(false)
+        return ls.removeItem('jwt')
+      }
+
+      dispatch(updateUser({ id: data.id, username: data.username, jwt }))
+      setLoading(false)
+    }
+
+    if (!jwt) {
+      setLoading(false)
+      return navigate(PublicRoutes.LOGIN.route)
+    }
+    recoveryUser()
+  }, [])
+  return { loading }
+}
