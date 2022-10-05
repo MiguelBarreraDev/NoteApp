0326ce71ca93c82d71d406f95684fdb268fde146
MiguelBarreraDev <barreram987@gmail.com>
2022-10-04 21:03:46 -0500

    Add: Show username of the user in this view

--- frontend/src/pages/Private/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Private/Dashboard/Dashboard.jsx
@@ -1,20 +1,32 @@
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import { useAuth } from '@/hooks'
 
 export default function Dashboard () {
+  const { logout, user } = useAuth()
+
   return (
     <Box
       sx={{
         width: '100%',
         height: theme => `calc(100vh - ${theme.components.Header.height})`,
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        gap: 2
       }}
     >
       <Typography variant='h1' color='Text.light'>
         Notes
       </Typography>
+      <Typography variant='h4' color='Text.light'>
+        {`"${user.username}"`}
+      </Typography>
+      <Button variant='outlined' onClick={() => logout()}>
+        LOGOUT
+      </Button>
     </Box>
   )
 }
