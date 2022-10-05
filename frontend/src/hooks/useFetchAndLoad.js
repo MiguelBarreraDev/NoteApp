c869c06758b9c50b5f3613970362ed8deea59c99
MiguelBarreraDev <barreram987@gmail.com>
2022-10-04 21:01:13 -0500

    Add: Handle error for Unauthenticated user

--- frontend/src/hooks/useFetchAndLoad.js
+++ frontend/src/hooks/useFetchAndLoad.js
@@ -1,17 +1,24 @@
+import { PublicRoutes } from '@/config'
+import {ls} from '@/utitlities'
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const useFetchAndLoad = () => {
   const [loading, setLoading] = useState(false)
   let controller = null
 
   const callEndpoint = async (asyncCall) => {
-    if (asyncCall.controller) controller = asyncCall.controller
     setLoading(true)
     let result = {}
+
+    if (asyncCall.controller) controller = asyncCall.controller
+
     try {
       result = await asyncCall.call
     } catch (error) {
-      console.log(error)
+      if (error.response.status === 401) {
+        ls.removeItem('jwt')
+      }
     }
     setLoading(false)
     return result
