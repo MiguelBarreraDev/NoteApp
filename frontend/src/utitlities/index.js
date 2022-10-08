00e373ff1fbe8b5524df99abd6d253720fff911f
MiguelBarreraDev <barreram987@gmail.com>
2022-10-08 01:40:45 -0500

    Fix: Rename utitlities module to utilities

--- frontend/src/utitlities/index.js
+++
@@ -1,5 +0,0 @@
-export { default as RoutesWithNotFound } from './RoutesWithNotFound'
-export { default as toList } from './toList'
-export { default as convertBase } from './convertBase'
-export { default as loadAbort } from './loadAbort.utility'
-export * from './globalObject'
