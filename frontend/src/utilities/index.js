164ca74de04141e8f80862164321386d54eb1beb
MiguelBarreraDev <barreram987@gmail.com>
2022-10-10 19:48:03 -0500

    Update: Rename loadAbort.utility.js file to load-abort.js

--- frontend/src/utilities/index.js
+++ frontend/src/utilities/index.js
@@ -1,5 +1,5 @@
 export { default as RoutesWithNotFound } from './RoutesWithNotFound'
 export { default as toList } from './toList'
 export { default as convertBase } from './convertBase'
-export { default as loadAbort } from './loadAbort.utility'
+export { default as loadAbort } from './loadAbort'
 export * from './globalObject'
