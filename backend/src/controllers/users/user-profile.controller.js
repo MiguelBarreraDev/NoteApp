c3371c6fec2b66ffc4d3120dafc292fa0c50fe81
MiguelBarreraDev <barreram987@gmail.com>
2022-10-11 04:28:30 -0500

    Add: Validation for existing user by id

--- backend/src/controllers/users/user-profile.controller.js
+++ backend/src/controllers/users/user-profile.controller.js
@@ -1,10 +1,15 @@
 import UserModel from "#schemas/user.schemas.js"
 
 const userPrfofileController = async (req, res) => {
-  const { id: userId } = req
-  const { name, username, surname } = await UserModel.findOne({ _id: userId }).exec()
+  const { id } = req 
 
-  return res.json({ id: userId, name, surname, username })
+  const existingUserById = await UserModel.findById(id).exec()
+  if (!existingUserById)
+    return res.send(401).json({ error: 'Unauthorized user' })
+
+  const { _id, name, surname, username } = existingUserById
+
+  return res.json({ id: _id, name, surname, username })
 }
 
 export default userPrfofileController
