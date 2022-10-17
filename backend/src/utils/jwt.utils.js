3b0611dea148a2cef3a2a21221b8c4c564246790
MiguelBarreraDev <barreram987@gmail.com>
2022-10-17 18:06:33 -0500

    Add: Utils directory for helper functions, classes, etc.

---
+++ backend/src/utils/jwt.utils.js
@@ -0,0 +1,17 @@
+import jsonwebtoken from "jsonwebtoken"
+
+const { JWT_PRIVATE_KEY } = process.env
+
+export const signJWT = ({ payload }) => {
+
+  const token = jsonwebtoken.sign(payload, JWT_PRIVATE_KEY, {
+    algorithm: 'HS256',
+    expiresIn: '1d'
+  })
+
+  return token
+}
+
+export const verifyJWT = ({ jwt }) => {
+  return jsonwebtoken.verify(jwt, JWT_PRIVATE_KEY)
+}
