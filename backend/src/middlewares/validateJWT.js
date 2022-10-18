26f95140338400df4a660b59b84f671bb8e415c4
MiguelBarreraDev <barreram987@gmail.com>
2022-10-18 18:59:13 -0500

    Style: Error format response

--- backend/src/middlewares/validateJWT.js
+++ backend/src/middlewares/validateJWT.js
@@ -3,7 +3,6 @@ import { verifyJWT } from '#utils/jwt.utils.js'
 const validateJWT = async (req, res, next) => {
   const message = 'Unauthorized user'
   const { authorization: jwt } = req.headers
-
   if (!jwt) return res.status(401).json({ message })
 
   try {
