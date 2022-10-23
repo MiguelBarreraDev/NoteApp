ad13a43c5c341a7cb1e7bad0ed91fdb53b746e38
MiguelBarreraDev <barreram987@gmail.com>
2022-10-23 12:06:49 -0500

    Update: Refactor with useMyForm custom hook

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useCallback } from 'react'
-import { signupService } from '@/services'
 import Typography from '@mui/material/Typography'
 import CircularProgress from '@mui/material/CircularProgress'
 import {
@@ -9,51 +8,67 @@ import {
   SubmitButton,
   CustomTextField
 } from '@/styledComponents'
-import { useAuth, useFetchAndLoad } from '@/hooks'
-import { createUser } from '@/redux/states'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { PrivateRoutes } from '@/config'
-import { ls } from '@/utilities'
+import { useAuth, useFetchAndLoad, useMyForm } from '@/hooks'
+import { toList } from '@/utilities'
 import { ShowError } from '@/components/ShowError'
-import { userAdapter } from '@/adapters'
 
 export default function SignUp () {
   const [errorMessage, setErrorMessage] = useState('')
-  const { logout, isLogged } = useAuth()
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { callEndpoint, loading } = useFetchAndLoad()
-  const [inputValues, setInputValues] = useState({
-    name: '',
-    surname: '',
-    username: '',
-    email: '',
-    password: ''
+  const { signup, logout, isLogged } = useAuth()
+  const { loading } = useFetchAndLoad()
+  const { getAttributes, formValues, customErrors, errors } = useMyForm({
+    name: { content: '' },
+    surname: { content: '' },
+    username: { content: '' },
+    email: { content: '', type: 'email' },
+    password: { content: '', type: 'password' }
   })
 
   useEffect(() => {
     isLogged && logout({ redirect: false })
   }, [])
 
-  const changeInputValue = inputKey => e => {
-    setInputValues(cs => ({ ...cs, [inputKey]: e.target.value }))
+  const handleErrors = () => {
+    const newErrors = {}
+
+    // Name errors
+    if (formValues.name === '') newErrors.name = 'Please complete this field'
+    else if (formValues.name < 2) newErrors.name = 'Min length 2 characters'
+    else newErrors.name = ''
+
+    // Surname errors
+    if (formValues.surname === '') newErrors.surname = 'Please complete this field'
+    else if (formValues.surname < 2) newErrors.surname = 'Min length 2 characters'
+    else newErrors.surname = ''
+
+    // Username errors
+    if (formValues.username === '') newErrors.username = 'Please complete this field'
+    else if (formValues.username < 8) newErrors.username = 'Min length 8 characters'
+    else newErrors.username = ''
+
+    // Email errors
+    if (formValues.email === '') newErrors.email = 'Please complete this field'
+    else if (formValues.email < 8) newErrors.email = 'Min length 8 characters'
+    else newErrors.email = ''
+
+    // Password errors
+    if (formValues.password === '') newErrors.password = 'Please complete this field'
+    else if (formValues.password < 8) newErrors.password = 'Min length 8 characters'
+    else newErrors.password = ''
+    console.log('newErrors:', newErrors)
+    customErrors(newErrors)
   }
 
   const handleClose = useCallback(() => setErrorMessage(''), [])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log('inputValues:', inputValues)
-    const response = await callEndpoint(signupService(inputValues))
 
-    if (response?.error) return setErrorMessage(response.error)
-    if (response?.data) {
-      const { data } = response
-      ls.setItem('jwt', data.jwt)
-      dispatch(createUser(userAdapter(data)))
-      navigate(PrivateRoutes.PRIVATE.route)
-    }
+    const existingErrors = toList(errors).some(error => error !== '')
+    if (existingErrors) return
+
+    const response = await signup(formValues)
+    if (response?.error) setErrorMessage(response.error)
   }
 
   return (
@@ -71,9 +86,8 @@ export default function SignUp () {
             required
             color='secondary'
             label='Name'
-            type='text'
-            value={inputValues?.name}
-            onChange={changeInputValue('name')}
+            {...getAttributes('name')}
+            onBlur={handleErrors}
           />
         </FormGridItem>
         <FormGridItem sm={5.8}>
@@ -81,9 +95,8 @@ export default function SignUp () {
             required
             color='secondary'
             label='Lastname'
-            type='text'
-            value={inputValues?.surname}
-            onChange={changeInputValue('surname')}
+            {...getAttributes('surname')}
+            onBlur={handleErrors}
           />
         </FormGridItem>
         <FormGridItem>
@@ -91,9 +104,8 @@ export default function SignUp () {
             required
             color='secondary'
             label='Username'
-            type='text'
-            value={inputValues?.username}
-            onChange={changeInputValue('username')}
+            {...getAttributes('username')}
+            onBlur={handleErrors}
           />
         </FormGridItem>
         <FormGridItem>
@@ -101,9 +113,8 @@ export default function SignUp () {
             required
             color='secondary'
             label='E-mail'
-            type='email'
-            value={inputValues?.email}
-            onChange={changeInputValue('email')}
+            {...getAttributes('email')}
+            onBlur={handleErrors}
           />
         </FormGridItem>
         <FormGridItem>
@@ -111,9 +122,8 @@ export default function SignUp () {
             required
             color='secondary'
             label='Password'
-            type='password'
-            value={inputValues?.password}
-            onChange={changeInputValue('password')}
+            {...getAttributes('password')}
+            onBlur={handleErrors}
           />
         </FormGridItem>
         <FormGridItem>
