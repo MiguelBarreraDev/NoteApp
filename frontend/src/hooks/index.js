6894822bf996334a34df5a32c31b314ff5159123
MiguelBarreraDev <barreram987@gmail.com>
2022-10-23 12:01:06 -0500

    Update: exports

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useFetchAndLoad } from './useFetchAndLoad'
 export { default as useAuth } from './useAuth'
 export { default as useRecoverUser } from './useRecoverUser'
+export { default as useMyForm } from './useMyForm'
