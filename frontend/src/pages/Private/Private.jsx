8c0499e467ea1845a76a96e1d2cbc7b7d369ce9a
MiguelBarreraDev <barreram987@gmail.com>
2022-11-07 21:26:30 -0500

    Add: Use provider of the notesContext context

--- frontend/src/pages/Private/Private.jsx
+++ frontend/src/pages/Private/Private.jsx
@@ -2,16 +2,19 @@ import { PrivateRoutes } from '@/config/routes'
 import { RoutesWithNotFound } from '@/utilities'
 import { lazy } from 'react'
 import { Route, Navigate } from 'react-router-dom'
+import { NotesContextProvider } from './context'
 
 const Dashboard = lazy(() => import('@/pages/Private/Dashboard/Dashboard'))
 const Profile = lazy(() => import('@/pages/Private/Profile/Profile'))
 
 export default function Private () {
   return (
-    <RoutesWithNotFound>
-      <Route path='/' element={<Navigate to={PrivateRoutes.DASHBOARD.route} />} />
-      <Route path={PrivateRoutes.DASHBOARD.route} element={<Dashboard />} />
-      <Route path={PrivateRoutes.PROFILE.route} element={<Profile />} />
-    </RoutesWithNotFound>
+    <NotesContextProvider>
+      <RoutesWithNotFound>
+        <Route path='/' element={<Navigate to={PrivateRoutes.DASHBOARD.route} />} />
+        <Route path={PrivateRoutes.DASHBOARD.route} element={<Dashboard />} />
+        <Route path={PrivateRoutes.PROFILE.route} element={<Profile />} />
+      </RoutesWithNotFound>
+    </NotesContextProvider>
   )
 }
