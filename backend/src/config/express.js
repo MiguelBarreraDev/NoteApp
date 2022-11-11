d65ffa46954b17f39ab16ff148f50c24679d96b6
MiguelBarreraDev <barreram987@gmail.com>
2022-11-10 20:42:45 -0500

    Add: cookieParser middleware

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -2,14 +2,17 @@ import userRouter from '#routes/user.routes.js'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
 
 const expressApp = express()
 
 // Middlewares
 expressApp.use(morgan('tiny'))
+expressApp.use(cookieParser())
 expressApp.use(express.json())
 expressApp.use(cors({
-  origin: process.env.CLIENT_URL
+  origin: process.env.CLIENT_URL,
+  credentials: true
 }))
 
 // Routes
