87b0dc08aab3d8c791e6965c28ec73ff1222b8b9
MiguelBarreraDev <barreram987@gmail.com>
2022-11-23 04:59:36 -0500

    Refactor: all methods and initialValues for state

--- frontend/src/hooks/useMyForm.js
+++ frontend/src/hooks/useMyForm.js
@@ -1,72 +1,110 @@
 import { toList } from '@/utilities'
 import { useEffect, useMemo, useState } from 'react'
 
-export default function useMyform (initialValues) {
-  const onlyContent = useMemo(() => Object
-    .keys(initialValues)
-    .reduce((obj, key) => ({ ...obj, [key]: initialValues[key].content }), {}),
-  [initialValues])
-
+/**
+ * Custom hooks that allows you to easily control forms
+ * params {Object} initialValues - default values for the inputs
+ */
+export default function useMyform (initialValues = {}) {
   const onlyKeys = useMemo(() => Object
     .keys(initialValues)
     .reduce((obj, key) => ({ ...obj, [key]: '' }), {}),
   [initialValues])
 
-  const [values, setValues] = useState(onlyContent)
+  // const ref = useRef()
+  const [refs, setRefs] = useState({})
+  const [values, setValues] = useState(initialValues)
   const [errors, setErrors] = useState(onlyKeys)
   const [validate, setValidate] = useState({ run: null })
   const [check, setCheck] = useState(false)
 
+  // Error validation on each update of 'errors' state
   useEffect(() => {
     const existingErrors = toList(errors).some(error => error !== '')
     existingErrors ? setCheck(false) : setCheck(true)
   }, [errors])
 
+  /**
+   * Sets attributes for an input element
+   * params {String} name - Key to register a input element
+   */
   const getAttributes = name => {
-    return {
-      type: initialValues[name].type ?? 'text',
+    if (!refs[name]) setRefs(cs => ({ ...cs, [name]: { current: null } }))
+
+    if (values[name] === undefined) setValues(cs => ({ ...cs, [name]: '' }))
+
+    const attributes = {
+      inputProps: { ref: refs[name] },
       onChange: e => handleChange(name, e),
       name,
-      value: values[name],
+      value: values[name] ?? '',
       error: Boolean(errors[name]),
       helperText: errors[name],
       onBlur: handleErrors
     }
+
+    return attributes
   }
 
+  /**
+   * Reset value inputs to initial values or empty object
+   */
   const reset = () => {
-    setValues(onlyContent)
+    setValues(initialValues || {})
   }
 
+  /**
+   * Allowed set function to validate values inputs
+   * params {Function} cb - Validate function
+   */
   const useValidate = (cb) => {
     useEffect(() => {
       setValidate({ run: cb })
-    }, [values])
+    }, [cb])
   }
 
+  /**
+   * Detec changes in the inputs and update your values
+   * params {String} name - name of the input
+   * params {object} e - onChange event
+   */
   const handleChange = (name, e) => {
     const { value } = e.target
 
     setValues(cs => ({ ...cs, [name]: value }))
   }
 
+  /**
+   * Validate all inputs for the onSubmit event
+   */
   const validateAllFields = () => {
     if (!validate.run) return { send: true }
+
     const newErrors = validate.run(values)
-    const existingErrors = Object.keys(errors).some(key => errors[key] !== newErrors[key])
+    const existingErrors = Object
+      .keys(errors)
+      .some(key => errors[key] !== newErrors[key])
 
     return { send: !existingErrors, newErrors }
   }
 
+  /**
+   * Update errors in the onBlur event
+   * params {object} e - onBlur event
+   */
   const handleErrors = e => {
-    const { name } = e.target
-
     if (!validate.run) return
 
+    const { name } = e.target
     const newErrors = validate.run(values)
+
     setErrors(cs => ({ ...cs, [name]: newErrors[name] }))
   }
 
+  /**
+   * HOF for handle onSubmit event
+   * params {Function} cb - Function to be executed with the inputs values
+   */
   const submit = (cb) => e => {
     e.preventDefault()
 
@@ -77,17 +115,12 @@ export default function useMyform (initialValues) {
     cb(values)
   }
 
-  const updateValidate = (name, value) => {
-    setValidate(cs => ({ ...cs, [name]: value }))
-  }
-
   return {
     getAttributes,
     values,
     errors,
     useValidate,
     submit,
-    updateValidate,
     reset
   }
 }
