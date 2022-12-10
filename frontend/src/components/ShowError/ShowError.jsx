f26612dc9002adf10b3654b27ba47293d71a44fb
MiguelBarreraDev <barreram987@gmail.com>
2022-12-09 19:12:59 -0500

    Refactor: Create BuiltSnackbar component

--- frontend/src/components/ShowError/ShowError.jsx
+++ frontend/src/components/ShowError/ShowError.jsx
@@ -10,36 +10,39 @@ const Alert = forwardRef((props, ref) => {
 
 Alert.displayName = 'Alert'
 
-export default function ShowError() {
-  const errorState = useSelector(state => state.error)
+const BuiltSnackbar = ({ error, index }) => {
   const dispatch = useDispatch()
 
   const handleClose = (errorId) => {
+    // if (errorState.some(error => error.id === errorId)) return
     dispatch(removeError({ errorId }))
   }
 
+  return <Snackbar
+    sx={{
+      width: '100%',
+      transform: `translateY(-${120 * index}%)`
+    }}
+    open={error.active}
+    autoHideDuration={6000}
+    onClose={() => handleClose(error.id)}
+  >
+    <Alert
+      variant='filled'
+      onClose={() => handleClose(error.id)}
+      severity={error.type}
+    >
+      {error.message}
+    </Alert>
+  </Snackbar>
+}
+
+export default function ShowError() {
+  const errorState = useSelector(state => state.error)
+
   return (
     <>
-      {errorState.map((error, index) => (
-        <Snackbar
-          key={error.id}
-          sx={{
-            width: '100%',
-            transform: `translateY(-${120 * index}%)`
-          }}
-          open={error.active}
-          autoHideDuration={6000}
-          onClose={() => handleClose(error.id)}
-        >
-          <Alert
-            variant='filled'
-            onClose={() => handleClose(error.id)}
-            severity={error.type}
-          >
-            {error.message}
-          </Alert>
-        </Snackbar>
-      ))}
+      {errorState.map((error, index) => <BuiltSnackbar key={error.id} error={error} index={index} />)}
     </>
   )
 }
